1332b29c1a0a669c9100dec1f553252153339254
Nils Koenig <nils@newk.it>
2018-05-31 11:18:55 +0200

    added pin layout, new NTP client; show date; slot machine effect

--- esp8266_nixie.ino
+++ esp8266_nixie.ino
@@ -5,28 +5,32 @@
 #include <SPI.h>
 #include <TimeLib.h>
 #include <Wire.h>
+#include <NtpClientLib.h>
+#include <Timer.h>
+Timer updateTime;
+Timer syncFromRTC;
+Timer doSlotMachine;
+Timer showDate;
+Timer queryNtp;
 
+// tested on a WEMOS D1 (retired model)
+//
+// PIN Layout Nixie Shield <-----> Wemos D1 ESP 8266
+//                     GND <-----> GND
+//                     VIN <-----> VIN
+//                     SCK <-----> D13/SCK/D5
+//                     MOSI<-----> D11/MOSI/D7
+//                     LE  <-----> D8
+//                     SDA <-----> D14/SDA
+//                     SCL <-----> D15/SCL
+// 
+// Not all functions of the shield (e.g, RBG LEDs, buzzer) are connected
 
 char ssid[] = "YourSSID";  //  your network SSID (name)
 char pass[] = "YourPassword";       // your network password
-
-
-unsigned int localPort = 2390;      // local port to listen for UDP packets
-
-/* Don't hardwire the IP address or we won't get the benefits of the pool.
-    Lookup the IP address for the host name instead */
-//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
-IPAddress timeServerIP; // time.nist.gov NTP server address
-const char* ntpServerName = "time.nist.gov";
-
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-
-// A UDP instance to let us send and receive packets over UDP
-WiFiUDP udp;
-
-unsigned long epoch;
+const char* ntpServerName = "pool.ntp.org"; // ntp server
+int8_t timeZone = 1;  // GMT offset [h]
+int8_t minutesTimeZone = 0; // GMT offset [min]
 
 const byte LEpin = 0; //pin Latch Enabled data accepted while HI level
 //const byte HIZpin = 0; //8 pin Z state in registers outputs (while LOW level)
@@ -46,18 +50,21 @@ byte dotPattern = B00000000; //bit mask for separeting dots
 byte zero = 0x00; //workaround for issue #527
 int RTC_hours, RTC_minutes, RTC_seconds, RTC_day, RTC_month, RTC_year, RTC_day_of_week;
 bool RTC_present;
-bool waitForNTPReply = false;
-uint32_t timeOffset = 1;
-int ntpRequestId;
+
+
+int updateTimerId;
 
 #define UpperDotsMask 0x80000000
 #define LowerDotsMask 0x40000000
 
-#include <Timer.h>
-Timer updateTime;
-Timer ntpRequest;
-Timer ntpReceive;
-Timer syncFromRTC;
+
+boolean syncEventTriggered = false; // True if a time even has been triggered
+NTPSyncEvent_t ntpEvent; // Last triggered event
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+unsigned long epoch;
 
 void doIndication()
 {
@@ -241,9 +248,29 @@ void getRTCTime()
 
 void funcUpdateTime()
 {
-  stringToDisplay = PreZero(hour() + timeOffset) + PreZero(minute()) + PreZero(second());
+  if (minute() % 2) {
+    stringToDisplay = PreZero(day()) + PreZero(month()) + (year()-2000);
+  } else {
+    stringToDisplay = PreZero(hour()) + PreZero(minute()) + PreZero(second());
+  }
   doIndication();
 }
+
+void slotMachine()
+{
+  for (int c = 0; c < 3; c++) {
+    for (int i = 0; i < 10; i++) {
+      stringToDisplay = "";
+      for (int j = 0; j < 6; j++) {
+        stringToDisplay.concat(i);
+      }
+      doIndication();
+      delay(30);
+    }
+  }
+}
+
+
 
 void setup()
 {
@@ -266,10 +293,6 @@ void setup()
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  Serial.println("Starting UDP");
-  udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(udp.localPort());
 
   // Port defaults to 8266
   // ArduinoOTA.setPort(8266);
@@ -306,6 +329,8 @@ void setup()
 
   Wire.begin();
 
+  slotMachine();
+
   getRTCTime();
   byte prevSeconds = RTC_seconds;
   unsigned long RTC_ReadingStartTime = millis();
@@ -322,135 +347,97 @@ void setup()
     }
   }
 
-  queryNTPServer();
+  NTP.onNTPSyncEvent ([](NTPSyncEvent_t event) {
+    ntpEvent = event;
+    syncEventTriggered = true;
 
-  updateTime.every(1000, funcUpdateTime);
+  });
+
+  NTP.begin("pool.ntp.org", timeZone, true, minutesTimeZone);
+  NTP.setInterval(10);  
+
+  updateTimerId = updateTime.every(1000, funcUpdateTime);
   if (RTC_present == true)
     syncFromRTC.every(60000, funcSyncFromRTC);
 
 
-  ntpRequestId = ntpRequest.every(60000, queryNTPServer);
+  //  ntpRequestId =
+  queryNtp.every(1000, queryNTPServer);
+
+
+  doSlotMachine.every(1800000, slotMachine);
+}
+
+void processSyncEvent (NTPSyncEvent_t ntpEvent) {
+  if (ntpEvent) {
+    Serial.print ("Time Sync error: ");
+    if (ntpEvent == noResponse)
+      Serial.println ("NTP server not reachable");
+    else if (ntpEvent == invalidAddress)
+      Serial.println ("Invalid NTP server address");
+  } else {
+        funcNtpToRtc();
+
+    Serial.print ("Got NTP time: ");
+    Serial.println (NTP.getTimeDateString (NTP.getLastNTPSync ()));
+  }
 }
 
 void queryNTPServer()
 {
-  //get a random server from the pool
-  WiFi.hostByName(ntpServerName, timeServerIP);
 
-  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-  // wait to see if a reply is available
-  waitForNTPReply = true;
-  ntpRequest.every(1000, getNTPAnswer, 10);
+  if (syncEventTriggered) {
+    processSyncEvent (ntpEvent);
+    syncEventTriggered = false;
+  }
 }
 
 void funcSyncFromRTC()
 {
   getRTCTime();
   setTime(RTC_hours, RTC_minutes, RTC_seconds, RTC_day, RTC_month, RTC_year);
-  Serial.println(F("RTC Sync"));
+ /* Serial.println(F("RTC Sync"));
+  Serial.println(RTC_hours);
+  Serial.println(RTC_minutes);
+  Serial.println(RTC_seconds);
+  Serial.println(RTC_day);
+  Serial.println(RTC_month);
+  Serial.println(RTC_year);*/
 }
 
-void getNTPAnswer()
+void funcNtpToRtc()
 {
-  int cb = udp.parsePacket();
-  if (!cb) {
-    Serial.println("no packet yet");
-  }
-  else {
-    Serial.print("packet received, length=");
-    Serial.println(cb);
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+  Serial.println(F("NTP SYNC"));
 
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
+  Serial.print (NTP.getTimeDateString ()); Serial.print (" ");
+  Serial.print (NTP.isSummerTime () ? "Summer Time. " : "Winter Time. ");
+  Serial.print ("WiFi is ");
+  Serial.print (WiFi.isConnected () ? "connected" : "not connected"); Serial.print (". ");
+  Serial.print ("Uptime: ");
+  Serial.print (NTP.getUptimeString ()); Serial.print (" since ");
+  Serial.println (NTP.getTimeDateString (NTP.getFirstSync ()).c_str ());
 
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = " );
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    //    uint8_t hour = (epoch  % 86400L) / 3600;
-    //    uint8_t minute = (epoch % 3600) / 60;
-    //    uint8_t second = epoch % 60;
-    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
-    // wait ten seconds before asking for the time again
-    uint8_t hour = (epoch  % 86400L) / 3600;
-    uint8_t minute = (epoch % 3600) / 60;
-    uint8_t second = epoch % 60;
-    setRTCDateTime(hour, minute, second, RTC_day, RTC_month, RTC_year, 1);
-    setTime(hour, minute, second, RTC_day, RTC_month, RTC_year);
-    //? set to RTC?
-    waitForNTPReply = false;
-    
-    Serial.println(F("NTP Sync"));
-
-    ntpReceive.stop(ntpRequestId);
-  }
+  byte y = year()-2000;
+  setRTCDateTime(hour(), minute(), second(), day(), month(), y, 1);
 
 }
+
+
+
 
 void loop()
 {
+
+
   ArduinoOTA.handle();
 
   updateTime.update();
   syncFromRTC.update();
-  ntpRequest.update();
-  if (waitForNTPReply == true)
-    ntpReceive.update();
+  doSlotMachine.update();
+  queryNtp.update();
+
+
 
 }
 
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); //NTP requests are to port 123
-  udp.write(packetBuffer, NTP_PACKET_SIZE);
-  udp.endPacket();
-}
 
